4ac03f2da4313c640dba9383b489ddacfa787a6f
TusharGirase <tushargirase1704@gmail.com>
2023-12-29 11:05:08 +0530

    Prevent account service to set balance in corrupt state

--- src/main/java/com/dws/challenge/service/AccountsService.java
+++ src/main/java/com/dws/challenge/service/AccountsService.java
@@ -1,74 +1,79 @@
 package com.dws.challenge.service;
 
-import java.math.BigDecimal;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.dws.challenge.domain.Account;
 import com.dws.challenge.domain.MoneyTransferRequest;
 import com.dws.challenge.exception.InvalidAccountIdExcption;
 import com.dws.challenge.exception.InvalidMoneyTransferRequest;
 import com.dws.challenge.repository.AccountsRepository;
-
+import java.math.BigDecimal;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class AccountsService {
 
-	@Getter
-	private final AccountsRepository accountsRepository;
+  @Getter private final AccountsRepository accountsRepository;
 
-	private final NotificationService notificationService;
+  private final NotificationService notificationService;
 
-	@Autowired
-	public AccountsService(AccountsRepository accountsRepository, NotificationService notificationService) {
-		this.accountsRepository = accountsRepository;
-		this.notificationService = notificationService;
-	}
+  @Autowired
+  public AccountsService(
+      AccountsRepository accountsRepository, NotificationService notificationService) {
+    this.accountsRepository = accountsRepository;
+    this.notificationService = notificationService;
+  }
 
-	public void createAccount(Account account) {
-		this.accountsRepository.createAccount(account);
-	}
+  public void createAccount(Account account) {
+    this.accountsRepository.createAccount(account);
+  }
 
-	public Account getAccount(String accountId) {
-		return this.accountsRepository.getAccount(accountId);
-	}
+  public Account getAccount(String accountId) {
+    return this.accountsRepository.getAccount(accountId);
+  }
 
-	public MoneyTransferResult transferMoney(MoneyTransferRequest moneyTransferRequest) {
-		Account accountFrom = accountsRepository.getAccount(moneyTransferRequest.getAccountFrom());
-		if (accountFrom == null) {
-			throw new InvalidAccountIdExcption(moneyTransferRequest.getAccountFrom());
-		}
-		BigDecimal accountFromUpdatedBalance = accountFrom.getBalance().subtract(moneyTransferRequest.getAmount());
+  public MoneyTransferResult transferMoney(MoneyTransferRequest moneyTransferRequest) {
+    Account accountFrom = accountsRepository.getAccount(moneyTransferRequest.getAccountFrom());
+    if (accountFrom == null) {
+      throw new InvalidAccountIdExcption(moneyTransferRequest.getAccountFrom());
+    }
+    Account accountTo = accountsRepository.getAccount(moneyTransferRequest.getAccountTo());
+    if (accountTo == null) {
+      throw new InvalidAccountIdExcption(moneyTransferRequest.getAccountTo());
+    }
 
-		if (accountFromUpdatedBalance.compareTo(BigDecimal.ZERO) < 0) {
-			throw new InvalidMoneyTransferRequest("Requested amount to transfer is overdrafting account.");
-		}
+    Object lock1 = accountFrom.getLOCK();
+    Object lock2 = accountTo.getLOCK();
 
-		Account accountTo = accountsRepository.getAccount(moneyTransferRequest.getAccountTo());
-		if (accountTo == null) {
-			throw new InvalidAccountIdExcption(moneyTransferRequest.getAccountTo());
-		}
+    if (accountFrom.hashCode() > accountTo.hashCode()) {
+      lock2 = accountFrom.getLOCK();
+      lock1 = accountTo.getLOCK();
+    }
+    synchronized (lock1) {
+      synchronized (lock2) {
+        accountFrom = accountsRepository.getAccount(moneyTransferRequest.getAccountFrom());
+        accountTo = accountsRepository.getAccount(moneyTransferRequest.getAccountTo());
 
-		Object lock1 = accountFrom.getLOCK();
-		Object lock2 = accountTo.getLOCK();
+        BigDecimal accountFromUpdatedBalance =
+            accountFrom.getBalance().subtract(moneyTransferRequest.getAmount());
 
-		if (accountFrom.hashCode() > accountTo.hashCode()) {
-			lock2 = accountFrom.getLOCK();
-			lock1 = accountTo.getLOCK();
-		}
-		synchronized (lock1) {
-			synchronized (lock2) {
-				BigDecimal accountToUpdatedBalance = accountTo.getBalance().add(moneyTransferRequest.getAmount());
-				accountsRepository.updateBalance(accountFrom.getAccountId(), accountFromUpdatedBalance);
-				accountsRepository.updateBalance(accountTo.getAccountId(), accountToUpdatedBalance);
-			}
-		}
-		notificationService.notifyAboutTransfer(accountFrom,
-				"Your account is debited with amount " + moneyTransferRequest.getAmount());
-		notificationService.notifyAboutTransfer(accountTo,
-				"Your account is credited with amount " + moneyTransferRequest.getAmount());
-		return MoneyTransferResult.SUCCESS;
-	}
+        if (accountFromUpdatedBalance.compareTo(BigDecimal.ZERO) < 0) {
+          throw new InvalidMoneyTransferRequest(
+              "Requested amount to transfer is overdrafting account.");
+        }
+
+        accountFromUpdatedBalance =
+            accountFrom.getBalance().subtract(moneyTransferRequest.getAmount());
+        BigDecimal accountToUpdatedBalance =
+            accountTo.getBalance().add(moneyTransferRequest.getAmount());
+        accountsRepository.updateBalance(accountFrom.getAccountId(), accountFromUpdatedBalance);
+        accountsRepository.updateBalance(accountTo.getAccountId(), accountToUpdatedBalance);
+      }
+    }
+    notificationService.notifyAboutTransfer(
+        accountFrom, "Your account is debited with amount " + moneyTransferRequest.getAmount());
+    notificationService.notifyAboutTransfer(
+        accountTo, "Your account is credited with amount " + moneyTransferRequest.getAmount());
+    return MoneyTransferResult.SUCCESS;
+  }
 }
--- src/test/java/com/dws/challenge/AccountsServiceTest.java
+++ src/test/java/com/dws/challenge/AccountsServiceTest.java
@@ -3,17 +3,17 @@ package com.dws.challenge;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.math.BigDecimal;
-
 import com.dws.challenge.domain.Account;
 import com.dws.challenge.domain.MoneyTransferRequest;
 import com.dws.challenge.exception.DuplicateAccountIdException;
 import com.dws.challenge.exception.InvalidAccountIdExcption;
 import com.dws.challenge.exception.InvalidMoneyTransferRequest;
 import com.dws.challenge.service.AccountsService;
-
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,91 +23,154 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest
 class AccountsServiceTest {
 
-	@Autowired
-	private AccountsService accountsService;
+  @Autowired private AccountsService accountsService;
 
-	@BeforeEach
-	public void setup() {
-		accountsService.getAccountsRepository().clearAccounts();
-	}
+  @BeforeEach
+  public void setup() {
+    accountsService.getAccountsRepository().clearAccounts();
+  }
 
-	@Test
-	void addAccount() {
-		Account account = new Account("Id-123");
-		account.setBalance(new BigDecimal(1000));
-		this.accountsService.createAccount(account);
+  @Test
+  void addAccount() {
+    Account account = new Account("Id-123");
+    account.setBalance(new BigDecimal(1000));
+    this.accountsService.createAccount(account);
 
-		assertThat(this.accountsService.getAccount("Id-123")).isEqualTo(account);
-	}
+    assertThat(this.accountsService.getAccount("Id-123")).isEqualTo(account);
+  }
 
-	@Test
-	void addAccount_failsOnDuplicateId() {
-		String uniqueId = "Id-" + System.currentTimeMillis();
-		Account account = new Account(uniqueId);
-		this.accountsService.createAccount(account);
+  @Test
+  void addAccount_failsOnDuplicateId() {
+    String uniqueId = "Id-" + System.currentTimeMillis();
+    Account account = new Account(uniqueId);
+    this.accountsService.createAccount(account);
 
-		try {
-			this.accountsService.createAccount(account);
-			fail("Should have failed when adding duplicate account");
-		} catch (DuplicateAccountIdException ex) {
-			assertThat(ex.getMessage()).isEqualTo("Account id " + uniqueId + " already exists!");
-		}
-	}
+    try {
+      this.accountsService.createAccount(account);
+      fail("Should have failed when adding duplicate account");
+    } catch (DuplicateAccountIdException ex) {
+      assertThat(ex.getMessage()).isEqualTo("Account id " + uniqueId + " already exists!");
+    }
+  }
 
-	@Test
-	void transferMoney_ifInvalidFromAccount() {
+  @Test
+  void transferMoney_ifInvalidFromAccount() {
 
-		String accountId1 = "Id-1";
-		String accountId2 = "Id-2";
-		Account account1 = new Account(accountId1, new BigDecimal("123.45"));
-		this.accountsService.createAccount(account1);
-		Account account2 = new Account(accountId2, new BigDecimal("253.33"));
-		this.accountsService.createAccount(account2);
+    String accountId1 = "Id-1";
+    String accountId2 = "Id-2";
+    Account account1 = new Account(accountId1, new BigDecimal("123.45"));
+    this.accountsService.createAccount(account1);
+    Account account2 = new Account(accountId2, new BigDecimal("253.33"));
+    this.accountsService.createAccount(account2);
 
-		try {
-			this.accountsService.transferMoney(MoneyTransferRequest.builder().accountFrom("Id-552")
-					.accountTo(accountId2).amount(new BigDecimal(200)).build());
-			fail("Should have failed when invalid account id passed");
-		} catch (InvalidAccountIdExcption ex) {
-			assertThat(ex.getMessage()).isEqualTo("Invalid account id provided Id-552");
-		}
-	}
+    try {
+      this.accountsService.transferMoney(
+          MoneyTransferRequest.builder()
+              .accountFrom("Id-552")
+              .accountTo(accountId2)
+              .amount(new BigDecimal(200))
+              .build());
+      fail("Should have failed when invalid account id passed");
+    } catch (InvalidAccountIdExcption ex) {
+      assertThat(ex.getMessage()).isEqualTo("Invalid account id provided Id-552");
+    }
+  }
 
-	@Test
-	void transferMoney_ifInvalidToAccount() {
+  @Test
+  void transferMoney_ifInvalidToAccount() {
 
-		String accountId1 = "Id-1";
-		String accountId2 = "Id-2";
-		Account account1 = new Account(accountId1, new BigDecimal("123.45"));
-		this.accountsService.createAccount(account1);
-		Account account2 = new Account(accountId2, new BigDecimal("253.33"));
-		this.accountsService.createAccount(account2);
+    String accountId1 = "Id-1";
+    String accountId2 = "Id-2";
+    Account account1 = new Account(accountId1, new BigDecimal("123.45"));
+    this.accountsService.createAccount(account1);
+    Account account2 = new Account(accountId2, new BigDecimal("253.33"));
+    this.accountsService.createAccount(account2);
 
-		try {
-			this.accountsService.transferMoney(MoneyTransferRequest.builder().accountFrom(accountId1)
-					.accountTo("Id-552").amount(new BigDecimal(100)).build());
-			fail("Should have failed when invalid account id passed");
-		} catch (InvalidAccountIdExcption ex) {
-			assertThat(ex.getMessage()).isEqualTo("Invalid account id provided Id-552");
-		}
-	}
+    try {
+      this.accountsService.transferMoney(
+          MoneyTransferRequest.builder()
+              .accountFrom(accountId1)
+              .accountTo("Id-552")
+              .amount(new BigDecimal(100))
+              .build());
+      fail("Should have failed when invalid account id passed");
+    } catch (InvalidAccountIdExcption ex) {
+      assertThat(ex.getMessage()).isEqualTo("Invalid account id provided Id-552");
+    }
+  }
 
-	@Test
-	void transferMoney_ifAccountOverdraft() {
+  @Test
+  void transferMoney_ifAccountOverdraft() {
 
-		String accountId1 = "Id-1";
-		String accountId2 = "Id-2";
-		Account account1 = new Account(accountId1, new BigDecimal("123.45"));
-		this.accountsService.createAccount(account1);
-		Account account2 = new Account(accountId2, new BigDecimal("253.33"));
-		this.accountsService.createAccount(account2);
+    String accountId1 = "Id-1";
+    String accountId2 = "Id-2";
+    Account account1 = new Account(accountId1, new BigDecimal("123.45"));
+    this.accountsService.createAccount(account1);
+    Account account2 = new Account(accountId2, new BigDecimal("253.33"));
+    this.accountsService.createAccount(account2);
 
-		try {
-			this.accountsService.transferMoney(MoneyTransferRequest.builder().accountFrom(accountId1)
-					.accountTo(accountId2).amount(new BigDecimal(200)).build());
-			fail("Should have failed when transfer request is overdraft");
-		} catch (InvalidMoneyTransferRequest ex) {
-			assertThat(ex.getMessage()).isEqualTo("Requested amount to transfer is overdrafting account.");
-		}
-	}
+    try {
+      this.accountsService.transferMoney(
+          MoneyTransferRequest.builder()
+              .accountFrom(accountId1)
+              .accountTo(accountId2)
+              .amount(new BigDecimal(200))
+              .build());
+      fail("Should have failed when transfer request is overdraft");
+    } catch (InvalidMoneyTransferRequest ex) {
+      assertThat(ex.getMessage())
+          .isEqualTo("Requested amount to transfer is overdrafting account.");
+    }
+  }
+
+  /**
+   * Tests deadlock situation as well as consistency of account
+   *
+   * @throws InterruptedException
+   */
+  @Test
+  @Timeout(5000)
+  public void testPotentialDeadlock() throws InterruptedException {
+    final Account account1 = new Account("Id-1000", new BigDecimal(1000));
+    this.accountsService.createAccount(account1);
+    final Account account2 = new Account("Id-2000", new BigDecimal(2000));
+    this.accountsService.createAccount(account2);
+
+    Thread thread1 =
+        new Thread(
+            () -> {
+              for (int i = 0; i < 1000; i++) {
+                accountsService.transferMoney(
+                    MoneyTransferRequest.builder()
+                        .accountFrom(account1.getAccountId())
+                        .accountTo(account2.getAccountId())
+                        .amount(new BigDecimal(1))
+                        .build());
+              }
+            });
+
+    Thread thread2 =
+        new Thread(
+            () -> {
+              for (int i = 0; i < 1000; i++) {
+                accountsService.transferMoney(
+                    MoneyTransferRequest.builder()
+                        .accountFrom(account2.getAccountId())
+                        .accountTo(account1.getAccountId())
+                        .amount(new BigDecimal(2))
+                        .build());
+              }
+            });
+
+    thread1.start();
+    thread2.start();
+
+    thread1.join();
+    thread2.join();
+
+    Assertions.assertEquals(
+        new BigDecimal(2000), accountsService.getAccount(account1.getAccountId()).getBalance());
+    Assertions.assertEquals(
+        new BigDecimal(1000), accountsService.getAccount(account2.getAccountId()).getBalance());
+  }
 }
